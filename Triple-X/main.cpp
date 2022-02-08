0b05f14a645b59fa9c4521c77b444ffd449154ba
pcnnadi <61913485+pcnnadi@users.noreply.github.com>
2022-02-08 16:34:03 +0000

    added prompts and basic functionality

--- Triple-X/main.cpp
+++ Triple-X/main.cpp
@@ -2,6 +2,18 @@
 
 int main()
 {
-	std::cout << "You are playing a guessing game to win $1,000,000..." << std::endl;
+	std::cout << "You are playing a guessing game to win $1,000,000..." << "\n";
+	std::cout << "\n" << "Enter the correct guess to proceed : " << "\n";
+
+	int a{ 4 };
+	int b{ 3 };
+	int c{ 2 };
+
+	int sum{ a + b + c };
+	int product{ a * b * c };
+
+	std::cout << "\n" << "The sum of its digits is " << sum << "\n";
+	std::cout << "\n" << "The product of its digits is " << product << std::endl;
+
 	return 0;
 }
